f2eb63931861ed4bfb55dc2393dc5533935577b8
Clueless Wizard <108606804+cluelesswizard@users.noreply.github.com>
2022-07-09 07:34:30 +0700

    Update Test Python.py

--- Test Python.py
+++ Test Python.py
@@ -1,4 +1,4 @@
-
+#  kode warna
 rt = '\033[0m'
 rd = '\033[31m'
 lcy = '\033[96m'
@@ -8,7 +8,6 @@ yl = '\033[93m'
 lrd = '\033[91m'
 pl = '\033[35m'
 
-
 print("\n[{}!{}]{}\tMasukkan Nama Anda  {}: {}Adam Zafron Zaman               {}[{}!{}] " .format(rd,rt,rd,rt,lcy,rt,rd,rt))
 print("[{}!{}]{}\tInput Nama Kelas    {}: {}Mekkah                          {}[{}!{}] " .format(rd,rt,rd,rt,lcy,rt,rd,rt))
 print("[{}!{}]{}\tInput Jurusan       {}: {}Pengembangan Perangkat Lunak    {}[{}!{}] " .format(rd,rt,rd,rt,lcy,rt,rd,rt))
@@ -17,8 +16,9 @@ nama = input("\n{}[{}?{}]{}\tNama \t{}:{}  " .format(rt,yl,rt,gr,rt,pl))
 kelas = input("{}[{}?{}]{}\tKelas \t{}:{}  " .format(rt,yl,rt,gr,rt,pl))
 jurusan = input("{}[{}?{}]{}\tJurusan {}:{}  " .format(rt,yl,rt,gr,rt,pl))
 
+# dekorasi
 print("\n\n{}{}===================={}      {}Hasil Data{}      {}===================={}\n" .format(rt,rd,rt,lcy,rt,rd,rt))
 
-print("\n<================> \t{}Nama Kamu    {}: " .format(gr,rt), nama,"\t\t""<================>" )
-print("<================> \t{}Kelas Kamu   {}: " .format(gr,rt), kelas,"\t\t\t\t""<================>" )
-print("<================> \t{}Jurusan Kamu {}: " .format(gr,rt), jurusan,"\t""<================>\n" )
+print("\n{}<================> \t{}Nama Kamu    {}: " .format(rt,gr,rd), nama,"\t\t""<================>", rt)
+print("{}<================> \t{}Kelas Kamu   {}: " .format(rt,gr,rd), kelas,"\t\t\t\t""<================>", rt)
+print("{}<================> \t{}Jurusan Kamu {}: " .format(rt,gr,rd), jurusan,"\t""<================>\n", rt)
